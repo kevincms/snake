3536de9643f17510d89e3e687f8e42d9073a6965
sentryc0201 <sentryc0201@naver.com>
2024-05-24 16:15:30 +0900

    create cnt_growth, cnt_poison, cnt_gate

--- snake.cpp
+++ snake.cpp
@@ -2,21 +2,19 @@
 #include <curses.h>
 #include <iostream>
 #include <Windows.h> // 지연능 위한 헤더파일
+#include "snake.h"
 
 using namespace std;
-
-struct Position // 좌표 저장 구조체
-{
-    int x, y;
-};
 
 class Snake {
 private:
     Position prev; // 꼬리가 이동전 있었던 좌표
     char direction; // 방향
+    int cnt_growth{ 0 }, cnt_poison{ 0 }, cnt_gate{ 0 };
 public:
     vector <Position> snake_body;
     Snake() {
+        prev = { 0, 0 };
         Position body; // default snake_body position 
         body = { 1, 2 };
         snake_body.push_back(body);
@@ -29,6 +27,7 @@ public:
 
     void set_direction() // 방향 입력받기
     {
+        Sleep(5000);
         int ch = getch();
         switch (ch) {
         case KEY_LEFT:
@@ -61,25 +60,32 @@ public:
             snake_body[0].x++;
             break;
         case 'u':
-            snake_body[0].y--;
+            snake_body[0].y++;
             break;
         case 'd':
-            snake_body[0].y++;
+            snake_body[0].y--;
             break;
         }
     }
 
     void eat_growth(const int x, const int y) // 성장 아이템 먹은지 판별, 작동
     {
-        if (snake_body[0].x == x && snake_body[0].y == y) snake_body.push_back(prev);
+        if (snake_body[0].x == x && snake_body[0].y == y) {
+            snake_body.push_back(prev);
+            cnt_growth++;
+        }
     }
 
     void eat_poison(const int x, const int y) // 독 아이템 먹은지 판별, 작동
     {
-        if (snake_body[0].x == x && snake_body[0].y == y) snake_body.pop_back();
+        if (snake_body[0].x == x && snake_body[0].y == y) {
+            snake_body.pop_back();
+            cnt_poison++;
+        }
     }
 
-    bool go_gate(int gate1[], int gate2[]) {
+    bool go_gate(const int gate1[], const int gate2[]) // 게이트 통과중인지 판별 + 이동
+    {
 
     }
 
@@ -87,6 +93,17 @@ public:
         return direction;
     }
 
+    int get_cnt_growth() {
+        return cnt_growth;
+    }
+
+    int get_cnt_poison() {
+        return cnt_poison;
+    }
+
+    int get_cnt_gate() {
+        return cnt_gate;
+    }
     void display() {
         for (int i = 0; i < snake_body.size(); i++) cout << "(" << snake_body[i].x << "," << snake_body[i].y << ") ";
         cout << endl;
@@ -101,13 +118,11 @@ int main() {
     nodelay(stdscr, true);  // 입력 대기 없이 바로 반환
 
     Snake s1;
-    Sleep(1000); // 시간 1초(1000ms)지연
+
     s1.set_direction();
     s1.display();
     s1.move();
-    s1.eat_poison(2, 2);
     s1.display();
-
 
     endwin();
     return 0;
