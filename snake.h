ccc01914fe7e8612c869c592b3b05623a39d2be6
sentryc0201 <sentryc0201@naver.com>
2024-05-27 15:26:12 +0900

    insert const

--- snake.h
+++ snake.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <curses.h>
-#include <Windows.h> // ������ ���� �������
+#include <chrono>
+#include <thread>
 #include "map.h"
 
 using namespace std;
@@ -9,18 +10,18 @@ class Snake {
 private:
     Position prev; // ������ �̵��� �־��� ��ǥ
     char direction; // ����
-    int cnt_growth{0}, cnt_poison{0}, cnt_gate{0};
+    int cnt_growth, cnt_poison, cnt_gate;
 public:
     vector <Position> snake_body;
     Snake();
     void set_direction();
     void move();
-    void eat_growth(const int x, const int y);
-    void eat_poison(const int x, const int y);
-    bool go_gate(const int gate1[], const int gate2[]);
+    void eat_growth(const Position item);
+    void eat_poison(const Position item);
+    bool go_gate(const Gate_Position gate1, const Gate_Position gate2);
     char get_direction();
-    int get_cnt_growth();
-    int get_cnt_poison();
-    int get_cnt_gate();
+    int get_cnt_growth() const;
+    int get_cnt_poison() const;
+    int get_cnt_gate() const;
     void display();
 };
